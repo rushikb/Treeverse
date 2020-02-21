9b06476f4502cf11be4e0005caa8ba1033043ea0
Stian Haklev <stian@minervaproject.com>
2020-02-21 09:16:00 +0100

    Adding ability to copy bullet list

--- src/viewer/tweet_tree.ts
+++ src/viewer/tweet_tree.ts
@@ -1,102 +1,104 @@
-import * as d3 from 'd3'
-import { TweetSet, Tweet } from './tweet_parser'
-
+import * as d3 from "d3";
+import { TweetSet, Tweet } from "./tweet_parser";
+import TurndownService from "turndown";
 
 export class TweetTree {
-    root: TweetNode
-    index: Map<string, TweetNode>
+  root: TweetNode;
+  index: Map<string, TweetNode>;
 
-    private constructor() { }
+  private constructor() {}
 
-    static fromTweetSet(tweetSet: TweetSet): TweetTree {
-        let tree = new TweetTree()
+  static fromTweetSet(tweetSet: TweetSet): TweetTree {
+    let tree = new TweetTree();
 
-        tree.index = new Map()
-        let { tweets, rootTweet } = tweetSet
+    tree.index = new Map();
+    let { tweets, rootTweet } = tweetSet;
 
-        for (let tweet of tweets) {
-            if (tweet.id == rootTweet) {
-                tree.root = new TweetNode(tweet)
-                tree.index.set(tweet.id, tree.root)
-                break
-            }
+    for (let tweet of tweets) {
+      if (tweet.id == rootTweet) {
+        tree.root = new TweetNode(tweet);
+        tree.index.set(tweet.id, tree.root);
+        break;
+      }
+    }
+
+    tree.addTweets(tweetSet);
+
+    return tree;
+  }
+
+  static fromRoot(root: TweetNode) {
+    let tree = new TweetTree();
+
+    tree.root = root;
+    return tree;
+  }
+
+  setCursor(tweetId: string, cursor: string) {
+    this.index.get(tweetId).cursor = cursor;
+  }
+
+  addTweets(tweetSet: TweetSet) {
+    let count = 0;
+    let { tweets, rootTweet, cursor } = tweetSet;
+
+    tweets.sort((a, b) => parseInt(a.id) - parseInt(b.id));
+
+    for (let tweet of tweets) {
+      if (!this.index.has(tweet.id)) {
+        count += 1;
+        let node = new TweetNode(tweet);
+        if (tweet.parent && this.index.has(tweet.parent)) {
+          this.index.get(tweet.parent).children.set(tweet.id, node);
         }
-
-        tree.addTweets(tweetSet)
-
-        return tree
+        this.index.set(tweet.id, node);
+      }
     }
 
-    static fromRoot(root: TweetNode) {
-        let tree = new TweetTree()
-
-        tree.root = root
-        return tree
+    if (cursor) {
+      this.index.get(rootTweet).cursor = cursor;
+    } else {
+      this.index.get(rootTweet).fullyLoaded = true;
     }
+    return count;
+  }
 
-    setCursor(tweetId: string, cursor: string) {
-        this.index.get(tweetId).cursor = cursor
-    }
-
-    addTweets(tweetSet: TweetSet) {
-        let count = 0
-        let { tweets, rootTweet, cursor } = tweetSet
-
-        tweets.sort((a, b) => parseInt(a.id) - parseInt(b.id))
-
-        for (let tweet of tweets) {
-            if (!this.index.has(tweet.id)) {
-                count += 1
-                let node = new TweetNode(tweet)
-                if (tweet.parent && this.index.has(tweet.parent)) {
-                    this.index.get(tweet.parent).children.set(tweet.id, node)
-                }
-                this.index.set(tweet.id, node)
-            }
-        }
-
-        if (cursor) {
-            this.index.get(rootTweet).cursor = cursor
-        } else {
-            this.index.get(rootTweet).fullyLoaded = true
-        }
-        return count
-    }
-
-    toHierarchy() {
-        return d3.hierarchy(this.root, (d: TweetNode) => Array.from(d.children.values()))
-    }
+  toHierarchy() {
+    return d3.hierarchy(this.root, (d: TweetNode) =>
+      Array.from(d.children.values())
+    );
+  }
 }
 
 /**
  * A tree node representing an individual tweet.
  */
 export class TweetNode {
-    children: Map<String, TweetNode>;
+  children: Map<String, TweetNode>;
 
-    tweet: Tweet;
-    cursor: string;
-    fullyLoaded: boolean;
+  tweet: Tweet;
+  cursor: string;
+  fullyLoaded: boolean;
 
-    constructor(tweet: Tweet) {
-        this.children = new Map<String, TweetNode>()
-        this.tweet = tweet
-    }
+  constructor(tweet: Tweet) {
+    this.children = new Map<String, TweetNode>();
+    this.tweet = tweet;
+  }
 
-    getId() {
-        return this.tweet.id
-    }
+  getId() {
+    return this.tweet.id;
+  }
 
-    /**
-     * Return false iff this tweet has more replies that we know about.
-     */
-    hasMore(): boolean {
-        // The fully loaded flag takes precedence because sometimes the
-        // reply count from twitter is greater than the number of tweets
-        // we actually get back from the API. This is probably because of
-        // replies from private accounts.
-        if (this.fullyLoaded) return false
-        if (this.cursor) return true
-        return this.children.size < this.tweet.replies
-    }
+  /**
+   * Return false iff this tweet has more replies that we know about.
+   */
+  hasMore(): boolean {
+    // The fully loaded flag takes precedence because sometimes the
+    // reply count from twitter is greater than the number of tweets
+    // we actually get back from the API. This is probably because of
+    // replies from private accounts.
+    if (this.fullyLoaded) return false;
+    if (this.cursor) return true;
+    return this.children.size < this.tweet.replies;
+  }
 }
--- src/viewer/visualization_controller.ts
+++ src/viewer/visualization_controller.ts
@@ -1,107 +1,147 @@
-import { FeedController } from './feed_controller'
-import { TweetVisualization } from './tweet_visualization'
-import { TweetNode, TweetTree } from './tweet_tree'
-import { TweetServer } from './tweet_server'
-import { Toolbar } from './toolbar'
-import { SerializedTweetNode } from './serialize'
-import * as d3 from 'd3'
-import { tree } from 'd3'
+import { FeedController } from "./feed_controller";
+import { TweetVisualization } from "./tweet_visualization";
+import { TweetNode, TweetTree } from "./tweet_tree";
+import { TweetServer } from "./tweet_server";
+import { Toolbar } from "./toolbar";
+import { SerializedTweetNode } from "./serialize";
+import * as d3 from "d3";
+import { tree } from "d3";
+
+declare global {
+  interface Navigator {
+    clipboard: any;
+  }
+}
+
+
+import TurndownService from "turndown";
+
+const turndownService = new TurndownService();
+
+const formatTweet = tweet =>
+  `@${tweet.username}: ${turndownService.turndown(tweet.bodyHtml)}\n`;
+
+const formatTweets = (node, indent = 0) => {
+  let out = "";
+  out += "  ".repeat(indent) + "- " + formatTweet(node.tweet);
+  console.log(node, indent, out);
+  if (node.children) {
+    out += Array.from(node.children)
+      .map(([k, v]) => formatTweets(v, indent + 1))
+      .join("\n");
+  }
+  return out;
+};
 
 export type PointNode = d3.HierarchyPointNode<TweetNode>;
 
-const expandText = 'Expand All'
-const cancelExpandText = 'Stop Expanding'
+const expandText = "Expand All";
+const cancelExpandText = "Stop Expanding";
 
 /**
  * The controller for the main tree visualization.
  */
 export class VisualizationController {
-    private tweetTree: TweetTree;
-    private vis: TweetVisualization;
-    private feed: FeedController;
-    private toolbar: Toolbar;
-    private server: TweetServer;
-    private expandingTimer: number;
-    private expandButton: HTMLButtonElement;
+  private tweetTree: TweetTree;
+  private vis: TweetVisualization;
+  private feed: FeedController;
+  private toolbar: Toolbar;
+  private server: TweetServer;
+  private expandingTimer: number;
+  private expandButton: HTMLButtonElement;
 
-    fetchTweets(tweetId: string) {
-        this.server.requestTweets(tweetId, null).then((tweetSet) => {
-            let tweetTree = TweetTree.fromTweetSet(tweetSet)
-            document.getElementsByTagName('title')[0].innerText =
-                `${tweetTree.root.tweet.username} - "${tweetTree.root.tweet.bodyText}" in Treeverse`
+  fetchTweets(tweetId: string) {
+    this.server.requestTweets(tweetId, null).then(tweetSet => {
+      let tweetTree = TweetTree.fromTweetSet(tweetSet);
+      document.getElementsByTagName(
+        "title"
+      )[0].innerText = `${tweetTree.root.tweet.username} - "${tweetTree.root.tweet.bodyText}" in Treeverse`;
 
-            this.setInitialTweetData(tweetTree)
-        })
-    }
+      this.setInitialTweetData(tweetTree);
+    });
+  }
 
-    setInitialTweetData(tree: TweetTree) {
-        this.tweetTree = tree
-        this.vis.setTreeData(tree)
-        this.vis.zoomToFit()
-    }
+  setInitialTweetData(tree: TweetTree) {
+    this.tweetTree = tree;
+    this.vis.setTreeData(tree);
+    this.vis.zoomToFit();
+  }
 
-    private expandNode(node: TweetNode, retry: boolean = true) {
-        this.server
-            .requestTweets(node.tweet.id, node.cursor)
-            .then((tweetSet) => {
-                let added = this.tweetTree.addTweets(tweetSet)
-                if (added > 0) {
-                    this.vis.setTreeData(this.tweetTree)
-                    if (node === this.tweetTree.root) {
-                        this.vis.zoomToFit()
-                    }    
-                } else if (retry) {
-                    this.expandNode(node, false)
-                }
-            })
-    }
-
-    shareClicked() {
-        let value = SerializedTweetNode.fromTweetNode(this.tweetTree.root)
-        chrome.runtime.sendMessage({ payload: value, message: 'share' })
-    }
-
-    expandOne() {
-        for (let tweetNode of this.tweetTree.index.values()) {
-            if (tweetNode.hasMore()) {
-                this.expandNode(tweetNode, true)
-                return
-            }
+  private expandNode(node: TweetNode, retry: boolean = true) {
+    this.server.requestTweets(node.tweet.id, node.cursor).then(tweetSet => {
+      let added = this.tweetTree.addTweets(tweetSet);
+      if (added > 0) {
+        this.vis.setTreeData(this.tweetTree);
+        if (node === this.tweetTree.root) {
+          this.vis.zoomToFit();
         }
-        this.stopExpanding()
+      } else if (retry) {
+        this.expandNode(node, false);
+      }
+    });
+  }
+
+  shareClicked() {
+    let value = SerializedTweetNode.fromTweetNode(this.tweetTree.root);
+    chrome.runtime.sendMessage({ payload: value, message: "share" });
+  }
+
+  expandOne() {
+    for (let tweetNode of this.tweetTree.index.values()) {
+      if (tweetNode.hasMore()) {
+        this.expandNode(tweetNode, true);
+        return;
+      }
+    }
+    this.stopExpanding();
+  }
+
+  stopExpanding() {
+    this.expandButton.textContent = expandText;
+    clearInterval(this.expandingTimer);
+    this.expandingTimer = null;
+  }
+
+  expandAll() {
+    if (this.expandingTimer === null) {
+      this.expandButton.textContent = cancelExpandText;
+      this.expandingTimer = setInterval(this.expandOne.bind(this), 1000);
+    } else {
+      this.stopExpanding();
+    }
+  }
+
+  copyClipboard() {
+    navigator.clipboard.writeText(formatTweets(this.tweetTree.root));
+  }
+
+  constructor(server: TweetServer, offline = false) {
+    this.server = server;
+    this.feed = new FeedController(document.getElementById("feedContainer"));
+    this.vis = new TweetVisualization(document.getElementById("tree"));
+    this.expandingTimer = null;
+
+    this.toolbar = new Toolbar(document.getElementById("toolbar"));
+
+    if (!offline) {
+      this.toolbar.addButton(
+        "Create shareable link",
+        this.shareClicked.bind(this)
+      );
+
+      this.expandButton = this.toolbar.addButton(
+        "Expand All",
+        this.expandAll.bind(this)
+      );
     }
 
-    stopExpanding() {
-        this.expandButton.textContent = expandText
-        clearInterval(this.expandingTimer)
-        this.expandingTimer = null
+    this.toolbar.addButton(
+      "Copy bullet list to clipboard",
+      this.copyClipboard.bind(this)
+    );
+    this.vis.on("hover", this.feed.setFeed.bind(this.feed));
+    if (!offline) {
+      this.vis.on("dblclick", this.expandNode.bind(this));
     }
-
-    expandAll() {
-        if (this.expandingTimer === null) {
-            this.expandButton.textContent = cancelExpandText
-            this.expandingTimer = setInterval(this.expandOne.bind(this), 1000)
-        } else {
-            this.stopExpanding()
-        }
-    }
-
-    constructor(server: TweetServer, offline = false) {
-        this.server = server
-        this.feed = new FeedController(document.getElementById('feedContainer'))
-        this.vis = new TweetVisualization(document.getElementById('tree'))
-        this.expandingTimer = null
-
-        this.toolbar = new Toolbar(document.getElementById('toolbar'))
-        if (!offline) {
-            this.toolbar.addButton('Create shareable link', this.shareClicked.bind(this))
-
-            this.expandButton = this.toolbar.addButton('Expand All', this.expandAll.bind(this))
-        }
-
-        this.vis.on('hover', this.feed.setFeed.bind(this.feed))
-        if (!offline) {
-            this.vis.on('dblclick', this.expandNode.bind(this))
-        }
-    }
+  }
 }
